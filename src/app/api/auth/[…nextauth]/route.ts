ddc17c5a449a1e091dd2195ee71f4881a8cd41ed
Rafles nainggolan <raflesngln@gmail.com>
2023-09-22 19:11:06 +0700

    configurasi JWT

---
+++ src/app/api/auth/[…nextauth]/route.ts
@@ -0,0 +1,33 @@
+// import { authenticate } from "@/services/authService"
+// import NextAuth from "next-auth"
+// import type { AuthOptions } from "next-auth"
+// import CredentialsProvider from "next-auth/providers/credentials"
+
+// export const authOptions: AuthOptions = {
+//   providers: [
+//     CredentialsProvider({
+//       name: 'Credentials',
+//       credentials: {
+//         email: { label: "Email", type: "text" },
+//         password: { label: "Password", type: "password" }
+//       },
+//       async authorize (credentials, req) {
+//         if (typeof credentials !== "undefined") {
+//           const res = await authenticate(credentials.email, credentials.password)
+//           if (typeof res !== "undefined") {
+//             return { ...res.user, apiToken: res.token }
+//           } else {
+//             return null
+//           }
+//         } else {
+//           return null
+//         }
+//       }
+//     })
+//   ],
+//   session: { strategy: "jwt" }
+// }
+
+// const handler = NextAuth(authOptions)
+
+// export { handler as GET, handler as POST }
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
-
+import toast from "react-hot-toast";
 
 type EnvVariableKey = "JWT_SECRET_KEY" | "JWT_EXPIRES_IN";
 
@@ -16,19 +16,55 @@ export function getEnvVariable(key: EnvVariableKey): string {
 }
 
 export function getErrorResponse(
-    status: number = 500,
-    message: string,
-    errors: ZodError | null = null
-  ) {
-    return new NextResponse(
-      JSON.stringify({
-        status: status < 500 ? "fail" : "error",
-        message,
-        errors: errors ? errors.flatten() : null,
-      }),
-      {
-        status,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
+  status: number = 500,
+  message: string,
+  errors: ZodError | null = null
+) {
+  return new NextResponse(
+    JSON.stringify({
+      status: status < 500 ? "fail" : "error",
+      message,
+      errors: errors ? errors.flatten() : null,
+    }),
+    {
+      status,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+}
+
+export function handleApiError(error: Error): void {
+  try {
+    let errorData;
+    try {
+      errorData = JSON.parse(error.message);
+    } catch (parseError) {
+      // Treat error.message as a plain error message
+      // console.log("Error message:", error.message);
+      toast.error(error.message);
+      return;
+    }
+
+    if (
+      typeof errorData === "object" &&
+      errorData !== null &&
+      "fieldErrors" in errorData
+    ) {
+      const fieldErrors = errorData.fieldErrors as Record<string, string[]>;
+      Object.keys(fieldErrors).forEach((fieldName) => {
+        const validationMessages = fieldErrors[fieldName];
+        if (validationMessages.length > 0) {
+          const firstValidationMessage = validationMessages[0];
+          toast.error(firstValidationMessage);
+          // console.log(
+          //   `Validation error for ${fieldName}:`,
+          //   firstValidationMessage
+          // );
+        }
+      });
+    }
+  } catch (error: any) {
+    // console.log("Original error message:", error);
+    toast.error(error);
   }
+}
